4b6fdc191e03116ca3c3a5aafb1fbefe46c7f3b7
Lamborjimmy <ertl.jaroslav38@gmail.com>
2026-03-13 18:52:55 +0100

    Refactoing package diagram visualizer

--- Assets/Scripts/Visualizers/PackageDiagramVisualizer.cs
+++ Assets/Scripts/Visualizers/PackageDiagramVisualizer.cs
@@ -1,7 +1,6 @@
 using Assets.Scripts.Data;
 using UnityEngine;
 using System.Collections.Generic;
-using System.Linq;
 using TMPro;
 
 namespace Assets.Scripts.Visualizers
@@ -10,159 +9,125 @@ namespace Assets.Scripts.Visualizers
     {
         protected override void DrawDiagramContent(GameObject container, List<NodeData> nodes, List<EdgeData> edges)
         {
-            GameObject nodesParent = new GameObject("Nodes");
-            GameObject edgesParent = new GameObject("Edges");
+            var (nodesParent, edgesParent) = CreateParentObjects(container);
+            NestingContext nesting = BuildNestingHierarchy(nodes, edges);
+            var nodeObjects = BuildNodes(nodesParent, nodes, nesting);
+            FilterAndRenderEdges(edges, nodeObjects, edgesParent.transform);
+        }
 
-            nodesParent.transform.SetParent(container.transform, false);
-            edgesParent.transform.SetParent(container.transform, false);
-
+        private Dictionary<string, GameObject> BuildNodes(GameObject nodesParent, List<NodeData> nodes, NestingContext nesting)
+        {
             var nodeObjects = new Dictionary<string, GameObject>();
 
-            // 1. Map Nesting Hierarchy
-            var parentToChildren = new Dictionary<string, List<NodeData>>();
-            var childToParent = new Dictionary<string, string>();
-            var nestedChildKeys = new HashSet<string>();
-
-            foreach (var edge in edges.Where(e => e.Type == DiagramEdgeTypes.NESTED))
-            {
-                string pKey = ExtractKeyFromId(edge.From);
-                string cKey = ExtractKeyFromId(edge.To);
-
-                nestedChildKeys.Add(cKey);
-                childToParent[cKey] = pKey;
-
-                if (!parentToChildren.ContainsKey(pKey))
-                    parentToChildren[pKey] = new List<NodeData>();
-
-                var childNode = nodes.FirstOrDefault(n => n.Key == cKey);
-                if (childNode != null)
-                    parentToChildren[pKey].Add(childNode);
-            }
-
-            var rootDiagram = nodes.FirstOrDefault(n => n.Type == DiagramNodeTypes.DIAGRAM && !nestedChildKeys.Contains(n.Key));
-
-            int GetDepth(string nodeKey)
-            {
-                int depth = 0;
-                string current = nodeKey;
-                while (childToParent.ContainsKey(current))
-                {
-                    current = childToParent[current];
-                    if (rootDiagram != null && current != rootDiagram.Key)
-                        depth++;
-                }
-                return depth;
-            }
-
-            // 2. Draw Nodes and Containers
             foreach (var node in nodes)
             {
-                if (node == rootDiagram) continue;
+                if (node == nesting.RootDiagram) continue;
 
-                int depth = GetDepth(node.Key);
+                int depth = nesting.GetDepth(node.Key);
                 float currentElevation = (depth + 1) * Y_ELEVATION;
 
-                GameObject nodeContainer = new GameObject("Node_" + (node.GetNodeName() ?? node.Key));
-                nodeContainer.transform.SetParent(nodesParent.transform, false);
+                string nodeLabel = "Node_" + (node.GetNodeName() ?? node.Key);
 
-                bool isContainer = parentToChildren.TryGetValue(node.Key, out var children) && children.Count > 0;
-                bool isPackage = isContainer || node.Type == DiagramNodeTypes.PACKAGE;
+                GameObject nodeContainer = CreateEmptyGameObject(nodesParent.transform, nodeLabel, Vector3.zero);
 
-                float currentTabHeight = isPackage ? 1.5f : 0f;
-
-                float nodeWidth, nodeHeight;
-                Vector3 position;
-
-                if (isContainer)
-                {
-                    GetBounds(node.Key, parentToChildren, out float minX, out float maxX, out float minZ, out float maxZ);
-
-                    float paddingX = 4.0f;
-                    float paddingZ = 4.5f;
-
-                    nodeWidth = (maxX - minX) + paddingX * 2;
-                    nodeHeight = (maxZ - minZ) + paddingZ * 2;
-
-                    float centerZ = (minZ + maxZ) / 2f;
-
-                    position = new Vector3((minX + maxX) / 2f, node.GetNodePosition().y + currentElevation - (Y_ELEVATION / 2f), centerZ + (currentTabHeight / 2f));
-                }
+                if (nesting.IsContainer(node.Key))
+                    BuildContainerNode(nodeContainer, node, nesting, currentElevation, depth);
+                else if (node.Type == DiagramNodeTypes.PACKAGE)
+                    BuildPackageNode(nodeContainer, node, currentElevation, depth);
                 else
-                {
-                    float textWidth = MeasureText(node.GetNodeName(), HEADER_FONT_SIZE, true);
-                    nodeWidth = Mathf.Max(textWidth + 3f, 6f);
-                    nodeHeight = 4f;
-
-                    float centerZ = node.GetNodePosition().z;
-                    position = new Vector3(node.GetNodePosition().x, node.GetNodePosition().y + currentElevation, centerZ + (currentTabHeight / 2f));
-                }
-
-                nodeContainer.transform.localPosition = position;
-
-                float totalHeight = nodeHeight + currentTabHeight;
-
-                GameObject visualObj;
-                if (isPackage && prefabsDictionary != null && prefabsDictionary.TryGetValue(DiagramNodeTypes.PACKAGE, out GameObject packagePrefab) && packagePrefab != null)
-                {
-                    visualObj = Object.Instantiate(packagePrefab, nodeContainer.transform);
-                }
-                else
-                {
-                    visualObj = GameObject.CreatePrimitive(PrimitiveType.Cube);
-                    visualObj.transform.SetParent(nodeContainer.transform, false);
-                }
-
-                visualObj.name = "Background";
-                visualObj.transform.localPosition = Vector3.zero;
-                visualObj.transform.localScale = new Vector3(nodeWidth, Y_ELEVATION, totalHeight);
-
-                if (isPackage && visualObj.transform.childCount > 0)
-                {
-                    float tabWorldWidth = Mathf.Min(2.5f, nodeWidth * 0.5f);
-                    Transform pkgBody = visualObj.transform.Find("Package") ?? visualObj.transform.Find("Background");
-                    if (pkgBody == null) pkgBody = visualObj.transform.GetChild(0);
-
-                    if (pkgBody != null)
-                    {
-                        pkgBody.localScale = new Vector3(1f, 1f, nodeHeight / totalHeight);
-                        pkgBody.localPosition = new Vector3(0f, 0f, -currentTabHeight / (2f * totalHeight));
-                    }
-
-                    Transform tabBody = visualObj.transform.Find("Tab");
-                    if (tabBody == null && visualObj.transform.childCount > 1) tabBody = visualObj.transform.GetChild(1);
-
-                    if (tabBody != null)
-                    {
-                        tabBody.localScale = new Vector3(tabWorldWidth / nodeWidth, 1f, currentTabHeight / totalHeight);
-                        float tabLocalX = -0.5f + (tabWorldWidth / (2f * nodeWidth));
-                        float tabLocalZ = 0.5f - (currentTabHeight / (2f * totalHeight));
-                        tabBody.localPosition = new Vector3(tabLocalX, 0f, tabLocalZ);
-                    }
-                }
-
-                Color layerColor = GetLayerColor(depth, isContainer);
-                foreach (var rend in visualObj.GetComponentsInChildren<Renderer>())
-                {
-                    if (rend.enabled)
-                    {
-                        rend.material = cachedNodeMaterial;
-                        rend.material.color = layerColor;
-                    }
-                }
-
-                float textZ = (nodeHeight / 2f) - (currentTabHeight / 2f) - 1.5f;
-                Vector3 textPos = new Vector3(0, Y_ELEVATION + Y_ELEVATION_TEXT_OFFSET, textZ);
-
-                CreateTextLabel(nodeContainer.transform, node.GetNodeName(), textPos, nodeWidth, HEADER_FONT_SIZE, TextAlignmentOptions.Top);
+                    BuildStandardNode(nodeContainer, node, currentElevation, depth);
 
                 nodeObjects[node.Key] = nodeContainer;
             }
 
-            var validEdges = edges.Where(e => e.Type != DiagramEdgeTypes.NESTED).ToList();
-            var selfLoops = validEdges.Where(e => ExtractKeyFromId(e.From) == ExtractKeyFromId(e.To));
-            var normalEdges = validEdges.Where(e => ExtractKeyFromId(e.From) != ExtractKeyFromId(e.To));
-            DrawDiagramEdges(selfLoops, nodeObjects, edgesParent, normalEdges);
+            return nodeObjects;
+        }
 
+        private void BuildContainerNode(GameObject nodeContainer, NodeData node, NestingContext nesting, float currentElevation, int depth)
+        {
+            GetRecursiveBounds(node.Key, nesting.ParentToChildren, out float minX, out float maxX, out float minZ, out float maxZ);
+
+            float paddingX = 4.0f;
+            float paddingZ = 4.5f;
+            float nodeWidth = (maxX - minX) + paddingX * 2;
+            float nodeHeight = (maxZ - minZ) + paddingZ * 2;
+
+            float currentTabHeight = 1.5f;
+            float totalHeight = nodeHeight + currentTabHeight;
+            float centerZ = (minZ + maxZ) / 2f;
+
+            nodeContainer.transform.localPosition = new Vector3((minX + maxX) / 2f, node.GetNodePosition().y + currentElevation - (Y_ELEVATION / 2f), centerZ + (currentTabHeight / 2f));
+
+            GameObject backgroundGroup = CreateEmptyGameObject(nodeContainer.transform, "Background", Vector3.zero);
+            GameObject visualsObj = CreateNodeGameObject(node.Type, backgroundGroup.transform, nodeWidth, totalHeight);
+
+            ConfigurePackagePrefabParts(visualsObj, nodeWidth, nodeHeight, totalHeight, currentTabHeight);
+            ApplyMaterialToHierarchy(visualsObj, GetLayerColor(depth, true));
+
+            float textZ = (nodeHeight / 2f) - (currentTabHeight / 2f) - 1.5f;
+            CreateTextLabel(backgroundGroup.transform, node.GetNodeName(), new Vector3(0, Y_ELEVATION + Y_ELEVATION_TEXT_OFFSET, textZ), nodeWidth, HEADER_FONT_SIZE, TextAlignmentOptions.Top, FontStyles.Bold);
+        }
+
+        private void BuildPackageNode(GameObject nodeContainer, NodeData node, float currentElevation, int depth)
+        {
+            float textWidth = MeasureText(node.GetNodeName(), HEADER_FONT_SIZE, true);
+            float nodeWidth = Mathf.Max(textWidth + 3f, 6f);
+            float nodeHeight = 4f;
+            float currentTabHeight = 1.5f;
+            float totalHeight = nodeHeight + currentTabHeight;
+
+            nodeContainer.transform.localPosition = new Vector3(node.GetNodePosition().x, node.GetNodePosition().y + currentElevation, node.GetNodePosition().z + (currentTabHeight / 2f));
+
+            GameObject backgroundGroup = CreateEmptyGameObject(nodeContainer.transform, "Background", Vector3.zero);
+            GameObject visualsObj = CreateNodeGameObject(node.Type, backgroundGroup.transform, nodeWidth, totalHeight);
+
+            ConfigurePackagePrefabParts(visualsObj, nodeWidth, nodeHeight, totalHeight, currentTabHeight);
+            ApplyMaterialToHierarchy(visualsObj, GetLayerColor(depth, false));
+
+            float textZ = (nodeHeight / 2f) - (currentTabHeight / 2f) - 1.5f;
+            CreateTextLabel(backgroundGroup.transform, node.GetNodeName(), new Vector3(0, Y_ELEVATION + Y_ELEVATION_TEXT_OFFSET, textZ), nodeWidth, HEADER_FONT_SIZE, TextAlignmentOptions.Top, FontStyles.Bold);
+        }
+
+        private void BuildStandardNode(GameObject nodeContainer, NodeData node, float currentElevation, int depth)
+        {
+            float textWidth = MeasureText(node.GetNodeName(), HEADER_FONT_SIZE, true);
+            float nodeWidth = Mathf.Max(textWidth + 3f, 6f);
+            float nodeHeight = 4f;
+
+            nodeContainer.transform.localPosition = new Vector3(node.GetNodePosition().x, node.GetNodePosition().y + currentElevation, node.GetNodePosition().z);
+
+            GameObject backgroundGroup = CreateEmptyGameObject(nodeContainer.transform, "Background", Vector3.zero);
+            GameObject visualsObj = CreateNodeGameObject(node.Type, backgroundGroup.transform, nodeWidth, nodeHeight);
+
+            ApplyMaterialToHierarchy(visualsObj, GetLayerColor(depth, false));
+
+            CreateTextLabel(backgroundGroup.transform, node.GetNodeName(), new Vector3(0, Y_ELEVATION + Y_ELEVATION_TEXT_OFFSET, 0), nodeWidth, HEADER_FONT_SIZE, TextAlignmentOptions.Center, FontStyles.Bold);
+        }
+
+        private void ConfigurePackagePrefabParts(GameObject visualObj, float nodeWidth, float nodeHeight, float totalHeight, float currentTabHeight)
+        {
+            if (visualObj.transform.childCount == 0) return;
+
+            float tabWorldWidth = Mathf.Min(2.5f, nodeWidth * 0.5f);
+            Transform pkgBody = visualObj.transform.Find("Package");
+            if (pkgBody == null) pkgBody = visualObj.transform.GetChild(0);
+
+            if (pkgBody != null)
+            {
+                pkgBody.localScale = new Vector3(1f, 1f, nodeHeight / totalHeight);
+                pkgBody.localPosition = new Vector3(0f, 0f, -currentTabHeight / (2f * totalHeight));
+            }
+
+            Transform tabBody = visualObj.transform.Find("Tab");
+            if (tabBody == null && visualObj.transform.childCount > 1) tabBody = visualObj.transform.GetChild(1);
+
+            if (tabBody != null)
+            {
+                tabBody.localScale = new Vector3(tabWorldWidth / nodeWidth, 1f, currentTabHeight / totalHeight);
+                float tabLocalX = -0.5f + (tabWorldWidth / (2f * nodeWidth));
+                float tabLocalZ = 0.5f - (currentTabHeight / (2f * totalHeight));
+                tabBody.localPosition = new Vector3(tabLocalX, 0f, tabLocalZ);
+            }
         }
 
         private Color GetLayerColor(int depth, bool isContainer)
@@ -181,30 +146,5 @@ namespace Assets.Scripts.Visualizers
             return c;
         }
 
-        private void GetBounds(string parentKey, Dictionary<string, List<NodeData>> parentToChildren, out float minX, out float maxX, out float minZ, out float maxZ)
-        {
-            minX = minZ = float.MaxValue;
-            maxX = maxZ = float.MinValue;
-
-            if (!parentToChildren.ContainsKey(parentKey)) return;
-
-            foreach (var child in parentToChildren[parentKey])
-            {
-                Vector3 pos = child.GetNodePosition();
-                minX = Mathf.Min(minX, pos.x);
-                maxX = Mathf.Max(maxX, pos.x);
-                minZ = Mathf.Min(minZ, pos.z);
-                maxZ = Mathf.Max(maxZ, pos.z);
-
-                if (parentToChildren.ContainsKey(child.Key))
-                {
-                    GetBounds(child.Key, parentToChildren, out float cMinX, out float cMaxX, out float cMinZ, out float cMaxZ);
-                    minX = Mathf.Min(minX, cMinX);
-                    maxX = Mathf.Max(maxX, cMaxX);
-                    minZ = Mathf.Min(minZ, cMinZ);
-                    maxZ = Mathf.Max(maxZ, cMaxZ);
-                }
-            }
-        }
     }
 }
